6ff43b9ecbea620ad18d0c9d84e893880e3b1c2b
Pe6e3 <xpe6e3@gmail.com>
2023-05-12 20:46:00 +0600

    Добавил основной репозиторий

---
+++ src/ItGeek.BLL/Repositories/GenericRepositoryAsync.cs
@@ -0,0 +1,47 @@
+﻿using ItGeek.DAL.Interfaces;
+using ItGeek.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItGeek.DAL.Data.Repositories
+{
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+    {
+        private readonly AppDbContext _db;
+
+        public GenericRepositoryAsync(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IReadOnlyList<T>> GetPagedAsync(int page, int size)
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+
+        public Task<T> GetByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+  
+
+        public Task<T> InsertAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ItGeek.DAL/Data/AppDbContext.cs
+++ src/ItGeek.DAL/Data/AppDbContext.cs
@@ -8,19 +8,21 @@ using System.Threading.Tasks;
 
 namespace ItGeek.DAL.Data
 {
-    internal class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         public DbSet<Author> Authors { get; set; }
-        public DbSet<AuthorsSocial> AuthorsSocial { get; set; }
+        public DbSet<AuthorsSocial> AuthorsSocials { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Comment> Comments { get; set; }    
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<Menu> Menus { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<Post> Posts { get; set; }
+        public DbSet<PostContent> PostContents { get; set; }
+        public DbSet<PostAuthor> PostAuthors { get; set; }
         public DbSet<PostCategory> PostCategories { get; set; }
         public DbSet<PostComment> PostComments { get; set; }
         public DbSet<PostTag> PostTags { get; set; }
@@ -29,5 +31,20 @@ namespace ItGeek.DAL.Data
         public DbSet<User> Users { get; set; }
         public DbSet<UserProfile> UserProfiles { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Menu>().HasData(new Menu
+            {
+                Name = "Меню в шапке",
+            });
+
+            modelBuilder.Entity<Menu>().HasData(new Menu
+            {
+                Name = "Меню в подвале",
+            });
+
+        }
     }
 }
