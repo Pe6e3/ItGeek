d37206985115df266d1cbd161c45182c48f79988
Pe6e3 <xpe6e3@gmail.com>
2023-05-17 20:20:11 +0600

    2 контроллера

--- src/ItGeek.Web/Areas/Admin/Controllers/TagsController.cs
+++ src/ItGeek.Web/Areas/Admin/Controllers/TagsController.cs
@@ -6,17 +6,73 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ItGeek.Web.Areas.Admin.Controllers
 {
-    public class TagsController : BaseController
+    public class TagsController : Controller
     {
 
         private readonly UnitOfWork _uow;
-        public TagsController(UnitOfWork uow) : base(uow)
+        public TagsController(UnitOfWork uow)
         {
             _uow = uow;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _uow.TagRepository.ListAllAsync());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _uow.TagRepository.GetByIDAsync(id));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            Tag tags = await _uow.TagRepository.GetByIDAsync(id);
+            if (tags != null)
+                await _uow.TagRepository.DeleteAsync(tags);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task<IActionResult> Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tags)
+        {
+            if (ModelState.IsValid)
+            {
+                await _uow.TagRepository.InsertAsync(tags);
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(tags);
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            Tag tags = await _uow.TagRepository.GetByIDAsync(id);
+            if (tags == null)
+            {
+                return NotFound();
+            }
+            return View(tags);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Tag tags)
+        {
+            if (ModelState.IsValid)
+            {
+                await _uow.TagRepository.UpdateAsync(tags);
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(tags);
         }
     }
 }
 
-//public TagsController(UnitOfWork uow, TagRepository tagRepository) : base(uow, tagRepository)
-//    {
-//}
+ 
