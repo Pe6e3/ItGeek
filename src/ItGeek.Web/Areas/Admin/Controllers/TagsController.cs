efd8d050fcc135eb91a85cd3c932e9ab40bb724c
Pe6e3 <xpe6e3@gmail.com>
2023-05-17 20:14:46 +0600

    Добавил Тэг контроллер

---
+++ src/ItGeek.Web/Areas/Admin/Controllers/TagsController.cs
@@ -0,0 +1,22 @@
+﻿using ItGeek.BLL;
+using ItGeek.BLL.Repositories;
+using ItGeek.DAL.Enitities;
+using ItGeek.Web.Areas.Admin.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItGeek.Web.Areas.Admin.Controllers
+{
+    public class TagsController : BaseController
+    {
+
+        private readonly UnitOfWork _uow;
+        public TagsController(UnitOfWork uow) : base(uow)
+        {
+            _uow = uow;
+        }
+    }
+}
+
+//public TagsController(UnitOfWork uow, TagRepository tagRepository) : base(uow, tagRepository)
+//    {
+//}
