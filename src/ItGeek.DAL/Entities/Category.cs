2a5461cfa6d960e54a19ae82a822f13ad1d1e130
Pe6e3 <xpe6e3@gmail.com>
2023-05-24 20:38:16 +0600

    Изменил тип Слага с Инт на Стр в Категориях

--- src/ItGeek.DAL/Entities/Category.cs
+++ src/ItGeek.DAL/Entities/Category.cs
@@ -11,7 +11,7 @@ namespace ItGeek.DAL.Entities
     {
         public string? Name{ get; set; }
         public string? Description { get; set; }
-        public int Slug { get; set; }
+        public string Slug { get; set; }
         public string? CategoryImage { get; set; }
 
                public List<Post>? Posts { get; } = new();
