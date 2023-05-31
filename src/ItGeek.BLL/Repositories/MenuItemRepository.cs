bde4df9238915fea346b99bccc9640e806824575
Pe6e3 <xpe6e3@gmail.com>
2023-05-31 19:07:43 +0600

    Меню и Пункты меню

--- src/ItGeek.BLL/Repositories/MenuItemRepository.cs
+++ src/ItGeek.BLL/Repositories/MenuItemRepository.cs
@@ -2,13 +2,19 @@
 using ItGeek.DAL.Data.Repositories;
 using ItGeek.DAL.Entities;
 using ItGeek.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace ItGeek.BLL.Repositories
 {
     public class MenuItemRepository : GenericRepositoryAsync<MenuItem>, IMenuItemRepository
     {
+        private readonly AppDbContext _db;
+
         public MenuItemRepository(AppDbContext db) : base(db)
         {
+            _db = db;
         }
+
+        public async Task<List<MenuItem>> GetByMenuIdAsync(int id) => await _db.MenuItems.Where(x => x.MenuId == id).ToListAsync();
     }
 }
--- src/ItGeek.DAL/Entities/MenuItem.cs
+++ src/ItGeek.DAL/Entities/MenuItem.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ItGeek.DAL.Entities
 {
-    public class MenuItem :BaseEntity
+    public class MenuItem :BaseEntity   
     {
-        public int MenuId { get; set; }
+        public int? MenuId { get; set; }
         public Menu? Menu { get; set; }
         public string? Name { get; set; }
         public string? Slug { get; set; }
