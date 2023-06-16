c0771242b478e5d630fbd79d4228cacc58e089da
Pe6e3 <xpe6e3@gmail.com>
2023-06-16 23:47:19 +0600

    Причесал оформление методов в PostRepository

--- src/ItGeek.BLL/Repositories/PostRepository.cs
+++ src/ItGeek.BLL/Repositories/PostRepository.cs
@@ -8,37 +8,39 @@ namespace ItGeek.BLL.Repositories;
 public class PostRepository : GenericRepositoryAsync<Post>, IPostRepository
 {
 
-    private readonly AppDbContext _db;
+    private readonly AppDbContext db;
 
     public PostRepository(AppDbContext db) : base(db)
     {
-        _db = db;
+        this.db = db;
     }
 
-    public async Task<Post> GetBySlugAsync(string slug)
-    {
-        return await _db.Posts.Include(x => x.Comments).Where(x => x.Slug == slug).Include(x => x.PostContents).FirstAsync();
-    }
+    public async Task<Post> GetBySlugAsync(string slug) => await
+    db.Posts
+        .Include(x => x.Comments)
+        .Where(x => x.Slug == slug)
+        .Include(x => x.PostContents)
+        .FirstAsync();
 
 
-    public async Task<List<Post>> GetLastAsync(int numberPosts)
-    {
-        //return await _db.Posts.OrderByDescending(x => x.Id).Take(numberPosts).DistinctBy(x => x.Categories.FirstOrDefault().Id).ToListAsync();
-        return await _db.Posts.Include(x => x.PostContents).Include(q => q.Categories).OrderByDescending(x => x.Id).Take(numberPosts).ToListAsync();
-    }
+    public async Task<List<Post>> GetLastAsync(int numberPosts) => await
+    db.Posts
+        .Include(x => x.PostContents)
+        .Include(q => q.Categories)
+        .OrderByDescending(x => x.Id)
+        .Take(numberPosts)
+        .ToListAsync();
     public async Task<List<Post>> ListByCategoryIdAsync(int categoryId)
     {
-
-        List<PostCategory> postCategory = await _db.PostCategories
+        List<PostCategory> postCategory = await
+        db.PostCategories
             .Where(x => x.CategoryId == categoryId)
             .ToListAsync();
 
         List<Post> post = new List<Post>();
 
         foreach (var pc in postCategory)
-        {
             post.Add(pc.Post);
-        }
 
         return post;
     }
@@ -46,15 +48,18 @@ public class PostRepository : GenericRepositoryAsync<Post>, IPostRepository
 
     public async Task<int> RandomPostId()
     {
-        Post? lastPost = await _db.Posts.LastOrDefaultAsync();
+        Post? lastPost = await db.Posts.LastOrDefaultAsync();
         int id = lastPost?.Id + 1 ?? 1; // Получаем Id последнего поста и прибавляем к нему 1
         return id;
     }
 
-    public async Task<List<Post>> ListWithIncludeAllAsync()
-    {
-        return await _db.Posts.Include(q => q.Categories).ToListAsync();
-    }
+    public async Task<List<Post>> ListWithIncludeAllAsync() => await
+    db.Posts
+        .Include(q => q.Categories)
+        .ToListAsync();
 
-    public async Task<List<Post>> ListPostsWithCatsAsync() => await _db.Posts.Include(x => x.Categories).ToListAsync();
+    public async Task<List<Post>> ListPostsWithCatsAsync() => await
+    db.Posts
+        .Include(x => x.Categories)
+        .ToListAsync();
 }
